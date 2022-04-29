99772dc545a3f399945e94d295da6234207c10fb
Lev <levnevinitsin@gmail.com>
2022-04-29 13:18:08 +0300

    Fix mistakes in code in content presenter

--- src/presenter/content-presenter.js
+++ src/presenter/content-presenter.js
@@ -2,7 +2,7 @@ import { FilterView, SortView, MainContentView, FilmListView, ShowMoreButtonView
 import { render } from 'utils';
 
 const FILMS_COUNT = 5;
-const RATING_FILMS = 2;
+const RATING_COUNT = 2;
 
 const RatingDescription = {
   TOP_RATED: 'Top rated',
@@ -15,18 +15,18 @@ export default class ContentPresenter {
 
   init = (siteMainElement) => {
     this.siteMainElement = siteMainElement;
-    render(new FilterView(), siteMainElement);
-    render(new SortView(), siteMainElement);
+    render(new FilterView(), this.siteMainElement);
+    render(new SortView(), this.siteMainElement);
     render(this.filmsListComponent, this.mainContentComponent.getElement());
     render(new ShowMoreButtonView(), this.filmsListComponent.getElement());
 
     render(
-      new FilmListView(RATING_FILMS, RatingDescription.TOP_RATED),
+      new FilmListView(RATING_COUNT, RatingDescription.TOP_RATED),
       this.mainContentComponent.getElement()
     );
 
     render(
-      new FilmListView(RATING_FILMS, RatingDescription.MOST_COMMENTED),
+      new FilmListView(RATING_COUNT, RatingDescription.MOST_COMMENTED),
       this.mainContentComponent.getElement()
     );
 
