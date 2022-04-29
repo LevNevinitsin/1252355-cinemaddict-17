95bd125ac40c73f7e0ac2cc8f0f28c9be70775af
Lev <levnevinitsin@gmail.com>
2022-04-29 11:14:11 +0300

    Refactor content presenter
    Moved constant values declarations out of the class

--- src/presenter/content-presenter.js
+++ src/presenter/content-presenter.js
@@ -1,27 +1,30 @@
 import { FilterView, SortView, MainContentView, FilmListView } from 'view';
 import {render} from '../render.js';
 
+const FILMS_COUNT = 5;
+const RATING_FILMS = 2;
+
+const RatingDescription = {
+  TOP_RATED: 'Top rated',
+  MOST_COMMENTED: 'Most commented',
+};
+
 export default class ContentPresenter {
-  filmsCount = 5;
-  topRatedFilmsCount = 2;
-  topRatedBlockTitle = 'Top rated';
-  mostCommentedFilmsCount = 2;
-  mostCommentedBlockTitle = 'Most commented';
   mainContentComponent = new MainContentView();
 
   init = (siteMainElement) => {
     this.siteMainElement = siteMainElement;
     render(new FilterView(), siteMainElement);
     render(new SortView(), siteMainElement);
-    render(new FilmListView(this.filmsCount), this.mainContentComponent.getElement());
+    render(new FilmListView(FILMS_COUNT), this.mainContentComponent.getElement());
 
     render(
-      new FilmListView(this.topRatedFilmsCount, this.topRatedBlockTitle),
+      new FilmListView(RATING_FILMS, RatingDescription.TOP_RATED),
       this.mainContentComponent.getElement()
     );
 
     render(
-      new FilmListView(this.mostCommentedFilmsCount, this.mostCommentedBlockTitle),
+      new FilmListView(RATING_FILMS, RatingDescription.MOST_COMMENTED),
       this.mainContentComponent.getElement()
     );
 
