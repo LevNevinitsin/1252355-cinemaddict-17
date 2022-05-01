829c0b259a9594e3138b743d4b6466b6b51d53d7
Lev <levnevinitsin@gmail.com>
2022-05-01 16:58:22 +0300

    Add dynamic data display to popup views

--- src/view/popup/popup-bottom-container-view.js
+++ src/view/popup/popup-bottom-container-view.js
@@ -1,16 +1,20 @@
 import { createElement } from 'utils';
 
-const createPopupBottomContainerTemplate = () => (
+const createPopupBottomContainerTemplate = (commentsCount) => (
   `<div class="film-details__bottom-container">
     <section class="film-details__comments-wrap">
-      <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">4</span></h3>
+      <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${commentsCount}</span></h3>
     </section>
   </div>`
 );
 
 export default class PopupBottomContainerView {
+  constructor(commentsCount) {
+    this.commentsCount = commentsCount;
+  }
+
   getTemplate() {
-    return createPopupBottomContainerTemplate();
+    return createPopupBottomContainerTemplate(this.commentsCount);
   }
 
   getElement() {
--- src/view/popup/popup-top-container-view.js
+++ src/view/popup/popup-top-container-view.js
@@ -1,80 +1,121 @@
-import { createElement } from 'utils';
+import { createElement, formatRating, formatDate, getHumanizedDuration } from 'utils';
 
-const createPopupTopContainerTemplate = () => (
-  `<div class="film-details__top-container">
-    <div class="film-details__close">
-      <button class="film-details__close-btn" type="button">close</button>
-    </div>
-    <div class="film-details__info-wrap">
-      <div class="film-details__poster">
-        <img class="film-details__poster-img" src="./images/posters/the-great-flamarion.jpg" alt="">
+const RELEASE_DATE_FORMAT = 'D MMMM YYYY';
 
-        <p class="film-details__age">18+</p>
-      </div>
-
-      <div class="film-details__info">
-        <div class="film-details__info-head">
-          <div class="film-details__title-wrap">
-            <h3 class="film-details__title">The Great Flamarion</h3>
-            <p class="film-details__title-original">Original: The Great Flamarion</p>
-          </div>
-
-          <div class="film-details__rating">
-            <p class="film-details__total-rating">8.9</p>
-          </div>
-        </div>
-
-        <table class="film-details__table">
-          <tr class="film-details__row">
-            <td class="film-details__term">Director</td>
-            <td class="film-details__cell">Anthony Mann</td>
-          </tr>
-          <tr class="film-details__row">
-            <td class="film-details__term">Writers</td>
-            <td class="film-details__cell">Anne Wigton, Heinz Herald, Richard Weil</td>
-          </tr>
-          <tr class="film-details__row">
-            <td class="film-details__term">Actors</td>
-            <td class="film-details__cell">Erich von Stroheim, Mary Beth Hughes, Dan Duryea</td>
-          </tr>
-          <tr class="film-details__row">
-            <td class="film-details__term">Release Date</td>
-            <td class="film-details__cell">30 March 1945</td>
-          </tr>
-          <tr class="film-details__row">
-            <td class="film-details__term">Runtime</td>
-            <td class="film-details__cell">1h 18m</td>
-          </tr>
-          <tr class="film-details__row">
-            <td class="film-details__term">Country</td>
-            <td class="film-details__cell">USA</td>
-          </tr>
-          <tr class="film-details__row">
-            <td class="film-details__term">Genres</td>
-            <td class="film-details__cell">
-              <span class="film-details__genre">Drama</span>
-              <span class="film-details__genre">Film-Noir</span>
-              <span class="film-details__genre">Mystery</span></td>
-          </tr>
-        </table>
-
-        <p class="film-details__film-description">
-          The film opens following a murder at a cabaret in Mexico City in 1936, and then presents the events leading up to it in flashback. The Great Flamarion (Erich von Stroheim) is an arrogant, friendless, and misogynous marksman who displays his trick gunshot act in the vaudeville circuit. His show features a beautiful assistant, Connie (Mary Beth Hughes) and her drunken husband Al (Dan Duryea), Flamarion's other assistant. Flamarion falls in love with Connie, the movie's femme fatale, and is soon manipulated by her into killing her no good husband during one of their acts.
-        </p>
-      </div>
-    </div>
-
-    <section class="film-details__controls">
-      <button type="button" class="film-details__control-button film-details__control-button--watchlist" id="watchlist" name="watchlist">Add to watchlist</button>
-      <button type="button" class="film-details__control-button film-details__control-button--active film-details__control-button--watched" id="watched" name="watched">Already watched</button>
-      <button type="button" class="film-details__control-button film-details__control-button--favorite" id="favorite" name="favorite">Add to favorites</button>
-    </section>
-  </div>`
+const createGenresTemplate = (genres) => (
+  genres.map((genre) => `<span class="film-details__genre">${genre}</span>`).join('')
 );
 
+const createPopupTopContainerTemplate = (film) => {
+  const {
+    title,
+    alternativeTitle,
+    totalRating,
+    poster,
+    ageRating,
+    director,
+    writers,
+    actors,
+    release,
+    runtime,
+    genre: genres,
+    description,
+  } = film.filmInfo;
+
+  const {
+    watchlist: isInWatchlist,
+    alreadyWatched: hasAlreadyWatched,
+    favorite: isFavorite
+  } = film.userDetails;
+
+  const rating = formatRating(totalRating);
+  const writersInfo = writers.join(', ');
+  const actorsInfo = actors.join(', ');
+  const releaseDate = formatDate(release.date, RELEASE_DATE_FORMAT);
+  const runtimeHumanized = getHumanizedDuration(runtime);
+  const country = release.releaseCountry;
+  const genresCaption = genres.length === 1 ? 'Genre' : 'Genres';
+  const genresTemplate = createGenresTemplate(genres);
+
+  const watchlistActiveClassName = isInWatchlist ? 'film-details__control-button--active' : '';
+  const alreadyWatchedActiveClassName = hasAlreadyWatched ? 'film-details__control-button--active' : '';
+  const favoriteActiveClassName = isFavorite ? 'film-details__control-button--active' : '';
+
+  return (
+    `<div class="film-details__top-container">
+      <div class="film-details__close">
+        <button class="film-details__close-btn" type="button">close</button>
+      </div>
+      <div class="film-details__info-wrap">
+        <div class="film-details__poster">
+          <img class="film-details__poster-img" src="${poster}" alt="">
+
+          <p class="film-details__age">${ageRating}+</p>
+        </div>
+
+        <div class="film-details__info">
+          <div class="film-details__info-head">
+            <div class="film-details__title-wrap">
+              <h3 class="film-details__title">${title}</h3>
+              <p class="film-details__title-original">Original: ${alternativeTitle}</p>
+            </div>
+
+            <div class="film-details__rating">
+              <p class="film-details__total-rating">${rating}</p>
+            </div>
+          </div>
+
+          <table class="film-details__table">
+            <tr class="film-details__row">
+              <td class="film-details__term">Director</td>
+              <td class="film-details__cell">${director}</td>
+            </tr>
+            <tr class="film-details__row">
+              <td class="film-details__term">Writers</td>
+              <td class="film-details__cell">${writersInfo}</td>
+            </tr>
+            <tr class="film-details__row">
+              <td class="film-details__term">Actors</td>
+              <td class="film-details__cell">${actorsInfo}</td>
+            </tr>
+            <tr class="film-details__row">
+              <td class="film-details__term">Release Date</td>
+              <td class="film-details__cell">${releaseDate}</td>
+            </tr>
+            <tr class="film-details__row">
+              <td class="film-details__term">Runtime</td>
+              <td class="film-details__cell">${runtimeHumanized}</td>
+            </tr>
+            <tr class="film-details__row">
+              <td class="film-details__term">Country</td>
+              <td class="film-details__cell">${country}</td>
+            </tr>
+            <tr class="film-details__row">
+              <td class="film-details__term">${genresCaption}</td>
+              <td class="film-details__cell">${genresTemplate}</td>
+            </tr>
+          </table>
+
+          <p class="film-details__film-description">${description}</p>
+        </div>
+      </div>
+
+      <section class="film-details__controls">
+        <button type="button" class="film-details__control-button film-details__control-button--watchlist ${watchlistActiveClassName}" id="watchlist" name="watchlist">Add to watchlist</button>
+        <button type="button" class="film-details__control-button film-details__control-button--watched ${alreadyWatchedActiveClassName}" id="watched" name="watched">Already watched</button>
+        <button type="button" class="film-details__control-button film-details__control-button--favorite ${favoriteActiveClassName}" id="favorite" name="favorite">Add to favorites</button>
+      </section>
+    </div>`
+  );
+};
+
 export default class PopupTopContainerView {
+  constructor(film) {
+    this.film = film;
+  }
+
   getTemplate() {
-    return createPopupTopContainerTemplate();
+    return createPopupTopContainerTemplate(this.film);
   }
 
   getElement() {
