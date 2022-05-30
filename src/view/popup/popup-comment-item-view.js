378ddb446bb541bbe83c47675f4d23be8f661ede
Lev <levnevinitsin@gmail.com>
2022-05-30 17:27:04 +0300

    Add HTML entities escaping

--- src/view/popup/popup-comment-item-view.js
+++ src/view/popup/popup-comment-item-view.js
@@ -1,3 +1,4 @@
+import he from 'he';
 import { AbstractView } from 'frameworkView';
 import { getRelativeTime } from 'utils';
 import { UserAction, UpdateType } from 'const';
@@ -20,7 +21,7 @@ const createPopupCommentItemTemplate = (comment) => {
         <img src="images/emoji/${emotion}.png" width="55" height="55" alt="emoji-${emotion}">
       </span>
       <div>
-        <p class="film-details__comment-text">${commentText}</p>
+        <p class="film-details__comment-text">${he.encode(commentText)}</p>
         <p class="film-details__comment-info">
           <span class="film-details__comment-author">${author}</span>
           <span class="film-details__comment-day">${relativeTime}</span>
